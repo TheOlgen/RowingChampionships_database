e39d9362a03c2969ccfa6130aa54d02c77d21b59
Olga R <80749258+TheOlgen@users.noreply.github.com>
2024-12-09 22:51:29 +0100

    Create inserts.sql

---
+++ init/inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO `klub` (`skrot_nazwy`, `pelna_nazwa`, `wzor_wiosel`) 
+  VALUES 
+    ('BTW', 'Bydgoskie Towarzystwo Wioślarskie', 'Niebieskie pióro z odwróconym szewronem z dwóch pasków: białego i czerwonego.'), 
+    ('RBW', 'Sekcja Wioślarska Klubu Sportowego „Posnania” w Poznaniu', 'Złoto-żółte pióro z 3 pionowymi pasami: białym-czerwonym-białym.'),
+    ('AZSS', 'Akademicki Związek Sportowy Organizacja Środowiskowa w Szczecinie', 'Czerwone pióro z białym paskiem w osi trzonu wiosła.'), 
+    ('AWFISG', ' Akademicki Związek Sportowy przy Akademii Wychowania Fizycznego i Sportu', 'Białe pióro z ciemnoniebieskim paskiem w osi trzonu wiosła.'), 
+    ('PTW', 'Płockie Towarzystwo Wioślarskie', 'Biały pas na górze, czerwony na dole, trójkątny ciemnoniebieski klin z prawej.'),
+    ('WTW', 'Warszawskie Towarzystwo Wioślarskie', 'Białe pióro z dwoma pionowymi ciemnoniebieskimi pasami.');
