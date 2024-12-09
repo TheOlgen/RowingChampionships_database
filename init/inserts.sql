560504b251c1cf9ecf6d1e6b1b7f8daa3031505b
Olga R <80749258+TheOlgen@users.noreply.github.com>
2024-12-09 23:02:31 +0100

    Update inserts.sql

--- init/inserts.sql
+++ init/inserts.sql
@@ -5,4 +5,8 @@ INSERT INTO `klub` (`skrot_nazwy`, `pelna_nazwa`, `wzor_wiosel`)
     ('AZSS', 'Akademicki Związek Sportowy Organizacja Środowiskowa w Szczecinie', 'Czerwone pióro z białym paskiem w osi trzonu wiosła.'), 
     ('AWFISG', ' Akademicki Związek Sportowy przy Akademii Wychowania Fizycznego i Sportu', 'Białe pióro z ciemnoniebieskim paskiem w osi trzonu wiosła.'), 
     ('PTW', 'Płockie Towarzystwo Wioślarskie', 'Biały pas na górze, czerwony na dole, trójkątny ciemnoniebieski klin z prawej.'),
-    ('WTW', 'Warszawskie Towarzystwo Wioślarskie', 'Białe pióro z dwoma pionowymi ciemnoniebieskimi pasami.');
+    ('WTW', 'Warszawskie Towarzystwo Wioślarskie', 'Białe pióro z dwoma pionowymi ciemnoniebieskimi pasami.')
+    ('TWDW', 'Towarzystwo Wisła dla Wioślarzy', 'Pomarańczowe pióro z białym szewronem i niebieskim trójkątem na końcówce'), 
+    ('PTWTP', 'Poznanskie Towarzystwo Wioslarskie Tryton Poznan', 'Ciemnoniebieskie pióro z białym pionowym pasem.'), 
+    ('WI', 'Wir Ilawa', 'Białe pióro z dwoma pionowymi pasami: zielonym i czerwonym.'), 
+    ('RTW', 'Regionalne Towarzystwo Wioślarskie Bydgostia Bydgoszcz', 'Białe pióro z trzema ukośnymi pasami: dwoma cienkimi czerwonymi, a w środku grubym ciemnoniebieskim.');
